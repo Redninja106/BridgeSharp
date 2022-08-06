ff020c6c73eadc610fe8562796483a0cb7c762bf
Ryan Andersen <redninja106@live.com>
2022-08-06 00:45:29 -0400

    add comment for stackOpKind

--- Bridge/StackOpKind.cs
+++ Bridge/StackOpKind.cs
@@ -1,5 +1,9 @@
 ﻿namespace Bridge;
 
+/// <summary>
+/// Specifies different kind of stack operations -- used as the first argument in an <see cref="Instruction{T}"/>
+/// or <see cref="Instruction{T1, T2}"/> for <see cref="OpCode.Push"/> or <see cref="OpCode.Pop"/> instructions.
+/// </summary>
 internal enum StackOpKind : byte
 {
     Const,
